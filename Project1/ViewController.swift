781ec21e483ce7000828456de95d55c13584f6ce
jwilson <jwilson@Annas-Mac-mini.udallas.edu>
2017-09-25 19:32:40 -0500

    finished project

--- Project1/ViewController.swift
+++ Project1/ViewController.swift
@@ -9,11 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Count: UILabel!
+    @IBOutlet weak var AddButton: UIButton!
+    @IBOutlet weak var SubButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func CountDown(_ sender: Any) {
+        guard let x = Count.text else {return}
+        guard let c = Int(x) else {return}
+        let n = String(c-1)
+        Count.text = n
+    }
+    
+    @IBAction func CountUp(_ sender: Any) {
+        guard let x = Count.text else {return}
+        guard let c = Int(x) else {return}
+        let n = String(c+1)
+        Count.text = n
+}
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
